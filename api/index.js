4759fafda17c105a16c2ae674c5c0fead15699ab
Narendran <narendran.kannan@freshworks.com>
2021-02-25 16:52:29 +0530

    reset data && Added Body Parser

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const app = require('express')()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const dataPath = './data/data.json';
 app.get('/api/getcount', (req, res) => {
   const json = require("../data/data.json");
